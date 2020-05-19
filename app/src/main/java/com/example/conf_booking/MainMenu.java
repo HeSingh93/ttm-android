a290acd8e804f3249baf79ae110e07a6af4fb8f5
hamnord <h.nordenstein@hotmail.com>
2020-05-19 13:31:43 +0200

    added basic navigation in mainmenu

--- app/src/main/java/com/example/conf_booking/MainMenu.java
+++ app/src/main/java/com/example/conf_booking/MainMenu.java
@@ -1,6 +1,9 @@
 package com.example.conf_booking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -9,11 +12,37 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainMenu extends AppCompatActivity {
 
     ViewFlipper v_flipper;
+    Button newBookingBtn, viewBookingBtn, myProfileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        newBookingBtn = findViewById(R.id.newBooking);
+        viewBookingBtn = findViewById(R.id.viewBooking);
+        myProfileBtn = findViewById(R.id.myProfile);
+
+        newBookingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, NewBooking.class));
+            }
+        });
+
+        viewBookingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, ViewBooking.class));
+            }
+        });
+
+        myProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( MainMenu.this, MyProfile.class));
+            }
+        });
 
         int[] images = new int[]{R.drawable.rum1, R.drawable.rum2, R.drawable.rum3, R.drawable.rum4};
 
