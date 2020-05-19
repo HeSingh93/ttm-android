9f8e65b753bc19ba1f3508ab1ff7079998da4446
hamnord <h.nordenstein@hotmail.com>
2020-05-19 13:30:59 +0200

    added basic navigation in mainmenu

---
+++ app/src/main/java/com/example/conf_booking/NewBooking.java
@@ -0,0 +1,14 @@
+package com.example.conf_booking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_booking);
+    }
+}
