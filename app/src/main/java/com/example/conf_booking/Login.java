c2ae3697ae64d0e7d4380fce6b37fc8acf675dab
hesingh <hermansingh1993@gmail.com>
2020-05-19 10:54:46 +0200

    added a button in signup and connected signup button in login screen to change the view

--- app/src/main/java/com/example/conf_booking/Login.java
+++ app/src/main/java/com/example/conf_booking/Login.java
@@ -1,15 +1,31 @@
 package com.example.conf_booking;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
+    Button signUpBtn,
+            loginBtn;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUpBtn = findViewById(R.id.signUp);
+        loginBtn = findViewById(R.id.login);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, SignUp.class));
+            }
+
+        });
     }
 }
