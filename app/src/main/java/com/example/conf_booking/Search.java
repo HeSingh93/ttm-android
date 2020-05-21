347fa96420e91ea808faaf18abd2a6aad887b67b
hamnord <h.nordenstein@hotmail.com>
2020-05-21 22:08:33 +0200

    modified Search.java

--- app/src/main/java/com/example/conf_booking/Search.java
+++ app/src/main/java/com/example/conf_booking/Search.java
@@ -2,6 +2,7 @@ package com.example.conf_booking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.SearchManager;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,10 @@ import android.content.Intent;
 import com.example.conf_booking.api.entities.SearchEntity;
 import com.example.conf_booking.api.retrofitservices.RetrofitHelper;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 import retrofit2.Call;
@@ -30,12 +35,13 @@ public class Search extends AppCompatActivity {
 
     TextView resultsTextView;
     ListView resultsListView;
-    String s="";
 
     EditText objectIdTextEdit,
             objectTypeTextEdit,
             fromDateTextEdit,
             toDateTextEdit;
+
+   ArrayAdapter adapter;
 
 
     @Override
@@ -43,32 +49,29 @@ public class Search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        search = findViewById(R.id.searchBtn);
-        search.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                searchPressed(v);
-            }
-        });
-
         objectIdTextEdit = findViewById(R.id.objectId);
         objectTypeTextEdit = findViewById(R.id.objectType);
         fromDateTextEdit = findViewById(R.id.fromDate);
         toDateTextEdit = findViewById(R.id.toDate);
         resultsListView = findViewById(R.id.resultsLV);
-        //resultsTextView = findViewById(R.id.resultsTV);
+
+        search = findViewById(R.id.searchBtn);
+        search.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) { searchPressed(view);}
+        });
 
     }
 
-
     public void searchPressed(final View view) {
-        SearchEntity newSearch = new SearchEntity();
 
-        newSearch.setObjectId(objectIdTextEdit.getText().toString().trim());
-        newSearch.setObjectType(objectTypeTextEdit.getText().toString().trim());
-        newSearch.setFromDate(fromDateTextEdit.getText().toString().trim());
-        newSearch.setToDate(toDateTextEdit.getText().toString().trim());
+        String objectId = objectIdTextEdit.getText().toString().trim();
+        String objectType = objectTypeTextEdit.getText().toString().trim();
+        String fromDate = fromDateTextEdit.getText().toString().trim();
+        String toDate = toDateTextEdit.getText().toString().trim();
+
+        SearchEntity newSearch = new SearchEntity(objectId,objectType,fromDate,toDate);
 
         Call<SearchEntity> call = RetrofitHelper.getAPIService().createSearch(newSearch);
         call.enqueue(new Callback<SearchEntity>() {
@@ -76,32 +79,26 @@ public class Search extends AppCompatActivity {
             @Override
             @EverythingIsNonNull
             public void onResponse(Call<SearchEntity> call, Response<SearchEntity> response) {
-                SearchEntity body = response.body();
+               SearchEntity body = response.body();
                 System.out.println(response.toString());
 
-              if(body!=null){
+              if(body != null){
                   Intent intent = new Intent(view.getContext(), Search.class);
                   intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                  String[] list = {body.toString()};
-                  System.out.println(body.toString());
+                  intent.putExtra("Search", body);
+                  List<String> list = new ArrayList<>();
 
+                  list.add(body.toString());
 
-                  ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),
-                          R.layout.support_simple_spinner_dropdown_item,
+                  adapter = new ArrayAdapter<String>(
+                          getBaseContext(),
+                          android.R.layout.simple_list_item_1,
                           list);
 
                   resultsListView.setAdapter(adapter);
 
-                  // Item onClick
-             /*     resultsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                      @Override
-                      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                          s=s+("\n")+((String) resultsListView.getItemAtPosition(position));
-                          resultsTextView.setText(s);
-                      }
-                  });
+                  System.out.println(body.toString());
 
-              */
 
               }else {
                   System.out.println("lol");
